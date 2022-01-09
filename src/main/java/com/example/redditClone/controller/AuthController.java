5cb2f0b2e5da57f8462dcfb300715fb15e6dd326
Joe Dorsey <joedorseyjr@gmail.com>
2022-01-09 11:39:38 -0500

    Add AuthService to AuthControllerj to send the signup email.

--- src/main/java/com/example/redditClone/controller/AuthController.java
+++ src/main/java/com/example/redditClone/controller/AuthController.java
@@ -1,18 +1,24 @@
 package com.example.redditClone.controller;
 
 import com.example.redditClone.dto.RegisterRequest;
+import com.example.redditClone.service.AuthService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.http.HttpStatus.OK;
+
 @RestController
 @RequestMapping("/api/auth")
 @AllArgsConstructor
 public class AuthController {
+    private final AuthService authService;
     @PostMapping ("/signup")
-    public void signup(@RequestBody RegisterRequest registerRequest){
-
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest){
+        authService.signup(registerRequest);
+        return new ResponseEntity<>("Account Activated Successfully", OK);
     }
 }
